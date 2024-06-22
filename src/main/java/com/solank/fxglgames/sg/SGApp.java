0c03d75283afe5786a58c0be2d0d9a4c07aca092
Soldatstar <h.logan.h@hotmail.com>
2024-06-22 15:26:06 +0200

    General refactoring of SGApp class

--- src/main/java/com/solank/fxglgames/sg/SGApp.java
+++ src/main/java/com/solank/fxglgames/sg/SGApp.java
@@ -2,78 +2,39 @@ package com.solank.fxglgames.sg;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
-import com.almasb.fxgl.app.scene.FXGLMenu;
-import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.audio.Music;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
 import com.almasb.fxgl.entity.SpawnData;
-import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.texture.Texture;
-import com.almasb.fxgl.ui.Position;
 import com.almasb.fxgl.ui.ProgressBar;
-import com.solank.fxglgames.sg.collision.BulletNoiseCollisionHandler;
-import com.solank.fxglgames.sg.collision.PlayerNoiseCollisionHandler;
-import com.solank.fxglgames.sg.components.PlayerComponent;
 import com.solank.fxglgames.sg.components.WeaponComponent;
-import com.solank.fxglgames.sg.ui.SGMainMenu;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
+import com.solank.fxglgames.sg.manager.ShootingManager;
 import javafx.util.Duration;
 
 import java.util.Map;
 import java.util.Random;
 
-import static com.almasb.fxgl.dsl.FXGL.addUINode;
-import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
-import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
-import static com.almasb.fxgl.dsl.FXGL.getGameScene;
-import static com.almasb.fxgl.dsl.FXGL.getGameWorld;
-import static com.almasb.fxgl.dsl.FXGL.getInput;
-import static com.almasb.fxgl.dsl.FXGL.getPhysicsWorld;
-import static com.almasb.fxgl.dsl.FXGL.getd;
-import static com.almasb.fxgl.dsl.FXGL.getdp;
-import static com.almasb.fxgl.dsl.FXGL.getip;
-import static com.almasb.fxgl.dsl.FXGL.inc;
-import static com.almasb.fxgl.dsl.FXGL.random;
-import static com.almasb.fxgl.dsl.FXGL.run;
-import static com.almasb.fxgl.dsl.FXGL.runOnce;
+import static com.almasb.fxgl.dsl.FXGL.*;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getAudioPlayer;
 
 public class SGApp extends GameApplication {
     public static final String VERSION = "0.0.3";
     public static final String TITLE = "SG";
     public static final double HEALTH_REGENRATE = 0.05;
-    private static final double COOLDOWN_DURATION = 1.0;
-    private static final double SHOT_PAUSE_DURATION = 0.15;
-    private static final String YUKINE_ENTITY = "Yukine";
-    private static final String HEALTH_ENTITY = "Health";
-    private static final String SCORE_ENTITY = "Score";
-    private static final Double YUKINE_MAX_HEALTH = 150.0;
+    public static final String YUKINE_ENTITY = "Yukine";
+    static final String HEALTH_ENTITY = "Health";
+    static final String SCORE_ENTITY = "Score";
+    static final Double YUKINE_MAX_HEALTH = 150.0;
     public static Random random = new Random();
     public static Entity yukine;
-    private double cdBetweenShots = 0.0;
+    private final com.solank.fxglgames.sg.init init = new init(this);
     private ProgressBar cooldownBar;
     private ProgressBar hpBar;
-    private boolean cooldown;
     private GameWorld gameWorld;
     private Music bgm;
-
-    private static void initScoreLabel() {
-        Label scoreLabel = new Label();
-        scoreLabel.setTextFill(Color.ORANGERED);
-        scoreLabel.setEffect(new DropShadow(15, Color.WHITE));
-        scoreLabel.setFont(Font.font(30.0));
-        scoreLabel.setAlignment(Pos.CENTER);
-        scoreLabel.textProperty().bind(getip(SCORE_ENTITY).asString("%d"));
-        addUINode(scoreLabel, getAppWidth() / 2, 2);
-    }
+    private ShootingManager shootingManager;
 
     public static void main(String[] args) {
         launch(args);
@@ -81,19 +42,8 @@ public class SGApp extends GameApplication {
 
     @Override
     protected void initSettings(GameSettings settings) {
-        settings.setTitle(TITLE);
-        settings.setVersion(VERSION);
-        settings.setWidth(1280);
-        settings.setHeight(720);
-        settings.setMainMenuEnabled(true);
-        settings.setGameMenuEnabled(false);
 
-        settings.setSceneFactory(new SceneFactory() {
-            @Override
-            public FXGLMenu newMainMenu() {
-                return new SGMainMenu();
-            }
-        });
+        init.initSettings(settings);
     }
 
     @Override
@@ -114,19 +64,19 @@ public class SGApp extends GameApplication {
         FXGL.getGameScene().setBackgroundRepeat(backgroundTexture.getImage());
         gameWorld.addEntityFactory(new SGFactory());
         gameWorld.create("Ground", new SpawnData());
-        yukine =
-            gameWorld.create(YUKINE_ENTITY, new SpawnData((double) getAppWidth() / 2, getAppHeight() - (double) 64));
+        yukine = gameWorld.create(YUKINE_ENTITY, new SpawnData((double) getAppWidth() / 2, getAppHeight() - (double) 64));
 
         yukine.addComponent(WeaponComponent.createWeapon());
 
         getGameScene().getViewport().setBounds(0, 0, Integer.MAX_VALUE, getAppHeight() + 100);
         getGameScene().getViewport().bindToEntity(yukine, getAppWidth() / 2, (getAppHeight() / 2) + 300);
-        //spawn 20 BGbuildings
+
+
         int x = 0;
         for (int i = 0; i < 50; i++) {
             x = 500 * i;
             int rnd = random.nextInt(100);
-            gameWorld.spawn("Cloud", new SpawnData(x, 100+rnd));
+            gameWorld.spawn("Cloud", new SpawnData(x, 100 + rnd));
         }
         gameWorld.spawn("Wall", new SpawnData(0, 0));
         gameWorld.spawn("Wall", new SpawnData(20000, 0));
@@ -136,141 +86,40 @@ public class SGApp extends GameApplication {
 
     @Override
     protected void initPhysics() {
-        getPhysicsWorld().setGravity(0, 600);
-        getPhysicsWorld().addCollisionHandler(new PlayerNoiseCollisionHandler());
-        getPhysicsWorld().addCollisionHandler(new BulletNoiseCollisionHandler());
+        init.initPhysics();
     }
 
     @Override
     protected void initUI() {
-        initScoreLabel();
-        initCooldownBar();
-        initHPBar();
+        init.initScoreLabel();
+        init.initCooldownBar();
+        init.initHPBar();
+        shootingManager = new ShootingManager(gameWorld, yukine, cooldownBar);
+
     }
 
     @Override
     protected void onUpdate(double tpf) {
         updateHealth();
-        cdBetweenShots += tpf;
         updateCooldownBar();
+        shootingManager.update(tpf);
         getPhysicsWorld().onUpdate(tpf);
 
         if (getd(HEALTH_ENTITY) <= 0) {
             gameOver(false);
         }
 
-        if (yukine.getX()>19800){
+        if (yukine.getX() > 19800) {
+            //TODO: Add real win condition
             gameOver(true);
         }
-
     }
 
     @Override
     protected void initInput() {
-
-        getInput().addAction(new UserAction("Move Right") {
-            @Override
-            protected void onAction() {
-                yukine.getComponent(PlayerComponent.class).right();
-            }
-
-            @Override
-            protected void onActionEnd() {
-                yukine.getComponent(PlayerComponent.class).stopMovingX();
-            }
-        }, KeyCode.D);
-
-        getInput().addAction(new UserAction("Move Left") {
-            @Override
-            protected void onAction() {
-                yukine.getComponent(PlayerComponent.class).left();
-            }
-
-            @Override
-            protected void onActionEnd() {
-                yukine.getComponent(PlayerComponent.class).stopMovingX();
-            }
-        }, KeyCode.A);
-
-        getInput().addAction(new UserAction("Jump") {
-            @Override
-            protected void onAction() {
-                yukine.getComponent(PlayerComponent.class).glide();
-
-            }
-
-            @Override
-            protected void onActionBegin() {
-                yukine.getComponent(PlayerComponent.class).jump();
-            }
-        }, KeyCode.SPACE);
-
-        getInput().addAction(new UserAction("Shoot") {
-            @Override
-            protected void onAction() {
-
-                if (cdBetweenShots >= SHOT_PAUSE_DURATION) {
-                    shoot();
-                }
-            }
-
-        }, MouseButton.PRIMARY);
-
-
-        getInput().addAction(new UserAction("Left") {
-            @Override
-            protected void onActionBegin() {
-                getInput().mockKeyPress(KeyCode.A);
-            }
-
-            @Override
-            protected void onActionEnd() {
-                getInput().mockKeyRelease(KeyCode.A);
-            }
-        }, KeyCode.LEFT);
-
-
-        getInput().addAction(new UserAction("Right") {
-            @Override
-            protected void onActionBegin() {
-                getInput().mockKeyPress(KeyCode.D);
-            }
-
-            @Override
-            protected void onActionEnd() {
-                getInput().mockKeyRelease(KeyCode.D);
-            }
-        }, KeyCode.RIGHT);
-
+        init.initInput();
     }
 
-    private void initHPBar() {
-        hpBar = new ProgressBar(false);
-        hpBar.setMinValue(0);
-        hpBar.setMaxValue(YUKINE_MAX_HEALTH);
-        hpBar.setCurrentValue(YUKINE_MAX_HEALTH);
-        hpBar.currentValueProperty().bind(getdp(HEALTH_ENTITY));
-        hpBar.setWidth(300);
-        hpBar.setLabelVisible(true);
-        hpBar.setLabelPosition(Position.RIGHT);
-        hpBar.setFill(Color.GREEN);
-        hpBar.setLayoutX(getAppWidth() - 390);
-        hpBar.setLayoutY(15);
-
-        getGameScene().addUINodes(hpBar);
-    }
-
-    private void initCooldownBar() {
-        cooldownBar = new ProgressBar(false);
-        cooldownBar.setMinValue(0);
-        cooldownBar.setMaxValue(100);
-        cooldownBar.prefWidth(200);
-        cooldownBar.setCurrentValue(100);
-        StackPane cooldownPane = new StackPane(cooldownBar);
-        cooldownPane.setLayoutX(20);
-        cooldownPane.setLayoutY(5);
-        getGameScene().addUINodes(cooldownPane);
-    }
 
     private void updateCooldownBar() {
         if (cooldownBar.getCurrentValue() < 100) {
@@ -285,56 +134,45 @@ public class SGApp extends GameApplication {
     }
 
     private void SpawnNoiseTop() {
-        int side = random(0, getAppWidth());
-        int x = side;
+        int x = random(0, getAppWidth());
         int y = 10;
         gameWorld.create("SmallNoise", new SpawnData(x + yukine.getX(), y).put(YUKINE_ENTITY, yukine));
     }
 
     private void SpawnNoiseSide() {
         int side = random(0, 1);
-        int appwidth = (getAppWidth() + 20) / 2;
-        int x = (int) yukine.getX() + appwidth;
+        int appWidth = (getAppWidth() + 20) / 2;
+        int x = (int) yukine.getX() + appWidth;
         int y = getAppHeight() - 100;
         if (side == 0) {
-            x = (int) yukine.getX() - appwidth;
+            x = (int) yukine.getX() - appWidth;
         }
         gameWorld.create("TallNoise", new SpawnData(x, y).put(YUKINE_ENTITY, yukine));
-    }
-
-    private void shoot() {
-        if (cooldown) {
-            return;
-        }
-
-        cdBetweenShots = 0.0;
-
-        gameWorld.create("Bullet", new SpawnData().put(YUKINE_ENTITY, yukine).put("mouseX", getInput().getMouseXWorld())
-            .put("mouseY", getInput().getMouseYWorld()));
-
-        cooldownBar.setCurrentValue(cooldownBar.getCurrentValue() - 20);
-
-        if (cooldownBar.getCurrentValue() < 2) {
-            cooldown = true;
-            runOnce(() -> {
-                cooldown = false;
-            }, Duration.seconds(COOLDOWN_DURATION));
-        }
     }
 
     private void gameOver(boolean reachedEndOfGame) {
         getInput().setRegisterInput(false);
         getInput().setProcessInput(false);
-        cooldown = false;
-        FXGL.getAudioPlayer().stopMusic(bgm);
-        StringBuilder builder = new StringBuilder();
-        builder.append("Game Over!\n\n");
-        if (reachedEndOfGame) {
-            builder.append("You have reached the end of the game!\n\n");
-            builder.append("Thank you for trying this Demo!\n\n");
-        }
-        builder.append("Final score: ")
-            .append(FXGL.geti(SCORE_ENTITY));
-        FXGL.getDialogService().showMessageBox(builder.toString(), () -> FXGL.getGameController().gotoMainMenu());
+        getAudioPlayer().stopMusic(bgm);
+    }
+
+    public ShootingManager getShootingManager() {
+        return shootingManager;
+    }
+
+    public ProgressBar getHpBar() {
+        return hpBar;
+    }
+
+    public void setHpBar(ProgressBar hpBar) {
+        this.hpBar = hpBar;
+    }
+
+    public ProgressBar getCooldownBar() {
+        return cooldownBar;
+    }
+
+    public void setCooldownBar(ProgressBar cooldownBar) {
+        this.cooldownBar = cooldownBar;
     }
 }
---
+++ src/main/java/com/solank/fxglgames/sg/init.java
@@ -0,0 +1,169 @@
+package com.solank.fxglgames.sg;
+
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.app.scene.FXGLMenu;
+import com.almasb.fxgl.app.scene.SceneFactory;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.ui.Position;
+import com.almasb.fxgl.ui.ProgressBar;
+import com.solank.fxglgames.sg.collision.BulletNoiseCollisionHandler;
+import com.solank.fxglgames.sg.collision.PlayerNoiseCollisionHandler;
+import com.solank.fxglgames.sg.components.PlayerComponent;
+import com.solank.fxglgames.sg.ui.SGMainMenu;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class init {
+    private final com.solank.fxglgames.sg.SGApp SGApp;
+
+    public init(com.solank.fxglgames.sg.SGApp SGApp) {
+        this.SGApp = SGApp;
+    }
+
+    protected void initInput() {
+        FXGL.getInput().addAction(new UserAction("Move Right") {
+            @Override
+            protected void onAction() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).right();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).stopMovingX();
+            }
+        }, KeyCode.D);
+
+        FXGL.getInput().addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).left();
+            }
+
+            @Override
+            protected void onActionEnd() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).stopMovingX();
+            }
+        }, KeyCode.A);
+
+        FXGL.getInput().addAction(new UserAction("Jump") {
+            @Override
+            protected void onAction() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).glide();
+            }
+
+            @Override
+            protected void onActionBegin() {
+                com.solank.fxglgames.sg.SGApp.yukine.getComponent(PlayerComponent.class).jump();
+            }
+        }, KeyCode.SPACE);
+
+        FXGL.getInput().addAction(new UserAction("Shoot") {
+            @Override
+            protected void onAction() {
+                SGApp.getShootingManager().shoot();
+
+            }
+        }, MouseButton.PRIMARY);
+
+        FXGL.getInput().addAction(new UserAction("Left") {
+            @Override
+            protected void onActionBegin() {
+                FXGL.getInput().mockKeyPress(KeyCode.A);
+            }
+
+            @Override
+            protected void onActionEnd() {
+                FXGL.getInput().mockKeyRelease(KeyCode.A);
+            }
+        }, KeyCode.LEFT);
+
+        FXGL.getInput().addAction(new UserAction("Right") {
+            @Override
+            protected void onActionBegin() {
+                FXGL.getInput().mockKeyPress(KeyCode.D);
+            }
+
+            @Override
+            protected void onActionEnd() {
+                FXGL.getInput().mockKeyRelease(KeyCode.D);
+            }
+        }, KeyCode.RIGHT);
+
+        //getInput().addAction(new UserAction("Jump") {
+        //    @Override
+        //    protected void onActionBegin() {
+        //        getInput().mockKeyPress(KeyCode.SPACE);
+        //    }
+
+        //    @Override
+        //    protected void onActionEnd() {
+        //        getInput().mockKeyRelease(KeyCode.SPACE);
+        //    }
+        //}, KeyCode.UP);
+    }
+
+    void initHPBar() {
+        SGApp.setHpBar(new ProgressBar(false));
+        SGApp.getHpBar().setMinValue(0);
+        SGApp.getHpBar().setMaxValue(com.solank.fxglgames.sg.SGApp.YUKINE_MAX_HEALTH);
+        SGApp.getHpBar().setCurrentValue(com.solank.fxglgames.sg.SGApp.YUKINE_MAX_HEALTH);
+        SGApp.getHpBar().currentValueProperty().bind(FXGL.getdp(com.solank.fxglgames.sg.SGApp.HEALTH_ENTITY));
+        SGApp.getHpBar().setWidth(300);
+        SGApp.getHpBar().setLabelVisible(true);
+        SGApp.getHpBar().setLabelPosition(Position.RIGHT);
+        SGApp.getHpBar().setFill(Color.GREEN);
+        SGApp.getHpBar().setLayoutX(FXGL.getAppWidth() - 390);
+        SGApp.getHpBar().setLayoutY(15);
+
+        FXGL.getGameScene().addUINodes(SGApp.getHpBar());
+    }
+
+
+    void initCooldownBar() {
+        SGApp.setCooldownBar(new ProgressBar(false));
+        SGApp.getCooldownBar().setMinValue(0);
+        SGApp.getCooldownBar().setMaxValue(100);
+        SGApp.getCooldownBar().prefWidth(200);
+        SGApp.getCooldownBar().setCurrentValue(100);
+        FXGL.getGameScene().addUINodes(SGApp.getCooldownBar());
+    }
+
+    void initScoreLabel() {
+        Label scoreLabel = new Label();
+        scoreLabel.setTextFill(Color.ORANGERED);
+        scoreLabel.setEffect(new DropShadow(15, Color.WHITE));
+        scoreLabel.setFont(Font.font(30.0));
+        scoreLabel.setAlignment(Pos.CENTER);
+        scoreLabel.textProperty().bind(FXGL.getip(com.solank.fxglgames.sg.SGApp.SCORE_ENTITY).asString("%d"));
+        FXGL.addUINode(scoreLabel, (double) FXGL.getAppWidth() / 2, 2);
+    }
+
+    protected void initSettings(GameSettings settings) {
+        settings.setTitle(com.solank.fxglgames.sg.SGApp.TITLE);
+        settings.setVersion(com.solank.fxglgames.sg.SGApp.VERSION);
+        settings.setWidth(1280);
+        settings.setHeight(720);
+        settings.setMainMenuEnabled(true);
+        settings.setGameMenuEnabled(false);
+
+        settings.setSceneFactory(new SceneFactory() {
+
+            @Override
+            public FXGLMenu newMainMenu() {
+                return new SGMainMenu();
+            }
+        });
+    }
+
+    protected void initPhysics() {
+        FXGL.getPhysicsWorld().setGravity(0, 600);
+        FXGL.getPhysicsWorld().addCollisionHandler(new PlayerNoiseCollisionHandler());
+        FXGL.getPhysicsWorld().addCollisionHandler(new BulletNoiseCollisionHandler());
+    }
+}
